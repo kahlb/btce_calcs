4bca1d68bb992edbf51c894b7b4eb72801ba4d05
kahlb <benediktkahl@gmail.com>
2013-11-16 16:58:56 +0100

    Update btce_buy.py

--- btce_buy.py
+++ btce_buy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.3
 # this software calculates a buy scenario
-# author toffifee, ltc donations welcome :) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
+# author kahlb, ltc donations welcome :) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
 budget = input("Enter the amount of currency you want to spend: ")
 price = input("Enter the price of the currency you want to buy: ")
 fee = 0.2
