f72783351f3d8fcb10a41dc37ced490806e53e92
kahlb <benediktkahl@gmail.com>
2013-11-16 16:59:31 +0100

    Update btce_goal.py

--- btce_goal.py
+++ btce_goal.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.3
 # this software calculates a targeted profit
-# author toffifee, ltc donations welcome =) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
+# author kahlb, ltc donations welcome =) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
 amount = float(input('Enter the amount of currency you want to trade with: '))
 goal = float(input("Enter the amount of currency you want to earn: "))
 price_sell = float(input("Enter the current price of the currency you want to increase: "))
