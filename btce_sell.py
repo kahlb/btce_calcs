0af064fae8f075b1967cf82ee98f5a857594e804
kahlb <benediktkahl@gmail.com>
2013-11-16 17:00:16 +0100

    Update btce_sell.py

--- btce_sell.py
+++ btce_sell.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.3
 # calculates the amount of currency you can buy with a given budget.
-# author toffifee, ltc donations welcome :) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
+# author kahlb, ltc donations welcome :) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
 budget = float(input("Enter the amount of currency you want to sell: "))
 price = float(input("Enter the price of the currency you want to earn: "))
 fee = 0.2
