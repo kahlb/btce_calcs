2d7a0296402e92f232b6a560512cf736c39bfa81
Benedikt Kahl <bkahl@debian-netbook.netbk>
2013-12-09 11:54:16 +0100

    New calc.

---
+++ btce_portfolio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.3
+# this software calculates the current value of your portfolio
+# author kahlb, ltc donations welcome :) LSRLVCerfwbAxBEyZJ4JtcZPdVwxc4zWs2
+fee=0.2
+feecalc=1-(fee/100)
+currnumber = input("Enter the number of different currencies in your portfolio: ")
+currlist = []
+pricelist = []
+for n in range(currnumber):
+	currlist.append(input("Enter the amout of currency {} in your portfolio: ".format(n+1)))
+for n in range(currnumber):
+	pricelist.append(input("Enter the price of currency {} to Bitcoin: ".format(n+1)))
+btcprice = input("Enter the current Bitcoin Price: ")
+portfolio = 0
+for n in range(currnumber):
+	portfolio = portfolio+(currlist[n]*pricelist[n]*feecalc)
+capital = portfolio*btcprice*feecalc
+print(portfolio)
+print('Your portfolio is worth {} (when transaction fee is {} percent)'.format(capital, fee))
+
+
